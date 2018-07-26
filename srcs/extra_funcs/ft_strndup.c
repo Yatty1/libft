897084ce67f82b5ee200821c9746a3f160297f55
Shinya Yamada <syamada@student.42.us.org>
2018-07-25 17:56:16 -0700

    fix strndup

--- srcs/extra_funcs/ft_strndup.c
+++ srcs/extra_funcs/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 21:42:34 by syamada           #+#    #+#             */
-/*   Updated: 2018/07/25 17:12:09 by syamada          ###   ########.fr       */
+/*   Updated: 2018/07/25 17:55:36 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ char	*ft_strndup(char *str, int i, int n)
 	if (!(dup = (char *)malloc(sizeof(str) * (n + 1))))
 		return (NULL);
 	while (j < n)
-	{
-		dup[j] = str[i];
-		j++;
-	}
+		dup[j++] = str[i++];
 	dup[j] = '\0';
 	return (dup);
 }
