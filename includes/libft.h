d608b1c49a6889168656e6f99ef3947798b03852
Shinya Yamada <syamada@student.42.us.org>
2018-07-26 19:31:58 -0700

    add ft_strchar

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 13:46:49 by syamada           #+#    #+#             */
-/*   Updated: 2018/07/25 17:34:54 by syamada          ###   ########.fr       */
+/*   Updated: 2018/07/26 19:31:40 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,5 +96,6 @@ void				ft_lstpush(t_list **alst, void *content, size_t size);
 int					ft_wordcount(char *s, char d);
 char				*ft_strjoin_with(const char *s1, const char *s2, char c);
 char				*ft_strndup(char *str, int i, int n);
+char				*ft_charstr(char c);
 
 #endif
