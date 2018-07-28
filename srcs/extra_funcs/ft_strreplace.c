0f4c512e749f50286534c14b53eb090136c7c4d6
Shinya Yamada <syamada@student.42.us.org>
2018-07-28 13:43:40 -0700

    fix strchr

--- srcs/extra_funcs/ft_strreplace.c
+++ srcs/extra_funcs/ft_strreplace.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_charstr.c                                       :+:      :+:    :+:   */
+/*   ft_strreplace.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/26 18:54:00 by syamada           #+#    #+#             */
-/*   Updated: 2018/07/27 22:32:47 by syamada          ###   ########.fr       */
+/*   Created: 2018/07/27 22:58:06 by syamada           #+#    #+#             */
+/*   Updated: 2018/07/27 23:00:54 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_charstr(char c)
-{
-	char	*str;
-	int		i;
+/*
+** replace string matching needle with s2.
+** return value is a fresh string.
+*/
 
-	i = 0;
-	str = ft_strnew(2);
-	str[i++] = c;
-	str[i] = '\0';
-	return (str);
+char	*ft_strreplace(const char *s1, const char *needle, const char *s2)
+{
 }
--- srcs/libc_funcs/ft_strchr.c
+++ srcs/libc_funcs/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 20:05:53 by syamada           #+#    #+#             */
-/*   Updated: 2018/07/27 18:05:33 by syamada          ###   ########.fr       */
+/*   Updated: 2018/07/28 13:43:24 by syamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char *v_s;
+	char *c_s;
 
-	v_s = (char *)s;
-	while (*v_s != c)
-		if (!*v_s++)
-			return (NULL);
-	return (v_s);
+	c_s = (char *)s;
+	while (*c_s)
+	{
+		if (*c_s == c)
+			return (c_s);
+		c_s++;
+	}
+	if (*c_s == c)
+		return (c_s);
+	return (NULL);
 }
